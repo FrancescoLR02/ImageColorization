9949e9f6520ae6b8af7b8b29cbb362c25529950d
Edoardo D'Amore <edoardo.damore@studenti.unipd.it>
2026-05-03 11:22:19 +0200

    bug fix

--- dpo_train.py
+++ dpo_train.py
@@ -218,7 +218,7 @@ def train(
                 l_batch,
                 chosen_ab,
                 rejected_ab,
-                pts=pts,
+                pts=pts.to(device),
                 beta=beta,
             )
             loss.backward()
